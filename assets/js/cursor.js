61f560ee590f54a39e35eae210ff6c0d7ef38d50
ayushmaninbox <ayushmanmohapatra895@gmail.com>
2024-10-28 13:11:29 +0530

    cursor movement of TOUCH DEVICES(mobile, tab, etc.) removed

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,19 +1,27 @@
 const cursor = document.getElementById("cursor");
 const trailElements = [];
 
-document.addEventListener("mousemove", (e) => {
-    cursor.style.left = `${e.clientX}px`;
-    cursor.style.top = `${e.clientY}px`;
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0));
+}
 
-    const trail = document.createElement("div");
-    trail.classList.add("trail");
-    trail.style.left = `${e.clientX}px`;
-    trail.style.top = `${e.clientY}px`;
-    document.body.appendChild(trail);
+if (!isTouchDevice()) {
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
 
-    trailElements.push(trail);
-    setTimeout(() => {
-        trail.remove();
-        trailElements.shift();
-    }, 500);
-});
+        const trail = document.createElement("div");
+        trail.classList.add("trail");
+        trail.style.left = `${e.clientX}px`;
+        trail.style.top = `${e.clientY}px`;
+        document.body.appendChild(trail);
+
+        trailElements.push(trail);
+        setTimeout(() => {
+            trail.remove();
+            trailElements.shift();
+        }, 500);
+    });
+}
